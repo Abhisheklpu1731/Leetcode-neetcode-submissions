7513023343aabf362418bdd44c466402e2d97bb8
Abhishek kumar singh <122448640+Abhisheklpu1731@users.noreply.github.com>
2026-09-05 18:15:00 +0530

    Add: binary-tree-right-side-view - submission-1

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+
+        def dfs(root, level):
+            if not root:
+                return
+
+            if level == len(res):
+                res.append(root.val)
+
+            dfs(root.right, level + 1)
+            dfs(root.left, level + 1)
+
+        dfs(root, 0)
+        return res
